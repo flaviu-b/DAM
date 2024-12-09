c5460c89bdeb3779f9408a497854fe88f995a553
Flaviu Mihai Brehariu <flaviumihaib@gmail.com>
2024-12-09 18:33:41 +0100

    añadido mkdocs

---
+++ src/com/iesElJust/edd/Calculadora.java
@@ -0,0 +1,9 @@
+package com.iesElJust.edd;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
---
+++ src/com/iesElJust/edd/Funciones.java
@@ -0,0 +1,29 @@
+package com.iesElJust.edd;
+
+public class Funciones {
+    
+    public static int Sumar (int num1, int num2) {
+    
+        return res;
+    } 
+
+    public static int Restar (int num1, int num2){
+    
+        return res;        
+    }
+
+    public static int Multiplicar (int num1, int num2){
+    
+        return res;
+
+    }
+
+    public static int Dividir (int num1, int num2){
+    
+        return res;
+        
+    }
+
+
+
+}
